856b693f850d72d215b0a226aaf1bd7211273907
Dilrabo1991 <dilraboxujamuratova@gmail.com>
2023-11-17 16:02:09 +0500

    so beatiful image in nature

--- src/components/API.js
+++ src/components/API.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
-const url = "https://api.unsplash.com/photos/?client_id=sKR5kUv3JyeeJd4B6jDV4aZCq3huC_dgKNpRiq3zAT8"
+const url = "https://api.unsplash.com/photos/random?client_id=sKR5kUv3JyeeJd4B6jDV4aZCq3huC_dgKNpRiq3zAT8&count=30&query=nature";//
 const API = () => {
     const [images, setImages] = useState([])
     const fetchApi = async () => {
@@ -19,21 +19,25 @@ const API = () => {
 
     return (
         <div className="container">
-            <br></br>
-            {/* <button onClick={fetchApi}>Fetch API</button> */}
-            <br></br>
-            <div className="photos" style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', justifyContent: 'center' }}>
+            <h1 style={{ textAlign: 'center', padding: '20px', color: 'white', backgroundColor: 'lightPink', borderRadius: '10px' }}>Unsplash API</h1>
+            <div className="photos" style={{ display: 'flex', flexWrap: 'wrap', gap: '5px', justifyContent: 'center' }}>
                 {images.length > 0 && (
                     images.map((image) => (
-                        <div className="photo" style={{ width: '500px', height: '400px', margin: '10px', objectFit: 'cover', overflow: 'hidden' }}>
+                        <div className="photo"
+                            key={image.id}
+                        >
                             <LazyLoadImage
-                                height={400}
-                                width={500}
+                                style={{
+                                    objectFit: 'cover',
+                                    borderRadius: '50px'
+                                }}
+                                height={200}
+                                width={300}
                                 src={image.urls.small}
                                 alt={image.description}
                                 key={image.id}
-                                effect='blur'
-                                placeholderSrc='url' />
+                                // effect='blur'
+                                placeholderSrc='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTl8TX1FqS5WQgfcqEjXU2ctTgEA665niePgg7sX8qdzPgS85wyD62DgPL2qn34v-_-xcc&usqp=CAU' />
 
                         </div>
                     ))
